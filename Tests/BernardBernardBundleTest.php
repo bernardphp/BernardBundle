3eecccd3f2a2a6cdd33f4ef74c2e4826d79153a7
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-01-23 17:48:14 +0100

    Test commands are registered

--- Tests/BernardBernardBundleTest.php
+++ Tests/BernardBernardBundleTest.php
@@ -5,17 +5,21 @@ namespace Bernard\BernardBundle\Tests;
 use Bernard\BernardBundle\BernardBernardBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Command\Command;
 
 class BernardBernardBundleTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->container = new ContainerBuilder;
+    }
+
     public function testCompilerPassIsRegistered()
     {
-        $container = new ContainerBuilder();
-
         $bundle = new BernardBernardBundle();
-        $bundle->build($container);
+        $bundle->build($this->container);
 
-        $passes = $container->getCompilerPassConfig()->getBeforeOptimizationPasses();
+        $passes = $this->container->getCompilerPassConfig()->getBeforeOptimizationPasses();
 
         $this->assertCount(2, $passes);
         $this->assertInstanceOf('Bernard\BernardBundle\DependencyInjection\Compiler\ReceiverPass', $passes[0]);
@@ -24,19 +28,18 @@ class BernardBernardBundleTest extends \PHPUnit_Framework_TestCase
 
     public function testCommandsAreRegistered()
     {
-        $command = $this->getMockBuilder('Symfony\Component\Console\Command\Command')
-            ->disableOriginalConstructor()->getMock();
-
-        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder');
-        $container->expects($this->at(0))->method('get')->with('bernard.consume_command')->will($this->returnValue($command));
-        $container->expects($this->at(1))->method('get')->with('bernard.produce_command')->will($this->returnValue($command));
+        $this->container->set('bernard.consume_command', new Command('bernard:consume'));
+        $this->container->set('bernard.produce_command', new Command('bernard:produce'));
 
         $kernel = $this->getMock('Symfony\Component\HttpKernel\KernelInterface');
-        $kernel->expects($this->any())->method('getContainer')->will($this->returnValue($container));
+        $kernel->expects($this->any())->method('getContainer')->will($this->returnValue($this->container));
 
         $application = new Application($kernel);
 
         $bundle = new BernardBernardBundle;
         $bundle->registerCommands($application);
+
+        $this->assertTrue($application->has('bernard:consume'));
+        $this->assertTrue($application->has('bernard:produce'));
     }
 }
