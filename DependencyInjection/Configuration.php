7e4119b2d1cea8427206336297b338e1791af612
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-01-24 10:11:36 +0100

    Update configuration with more validation also test more of the configuration options

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,10 +12,28 @@ class Configuration implements \Symfony\Component\Config\Definition\Configuratio
         $tree = new TreeBuilder();
         $root = $tree->root('bernard_bernard');
 
+        $drivers = array('file', 'predis', 'doctrine');
+        $serializers = array('simple', 'jms', 'symfony');
+
         $root
+            ->validate()
+                ->ifTrue(function ($v) { return 'file' === $v['driver'] && empty($v['directory']); })
+                ->thenInvalid('The "directory" option must be defined when using the file driver.')
+            ->end()
             ->children()
-                ->scalarNode('driver')->isRequired()->end()
-                ->scalarNode('serializer')->defaultValue('simple')->end()
+                ->scalarNode('driver')
+                    ->validate()
+                        ->ifNotInArray($drivers)
+                        ->thenInvalid('The driver %s is not supported. Please choose one of ' . json_encode($drivers))
+                    ->end()
+                ->end()
+                ->scalarNode('serializer')
+                    ->defaultValue('simple')
+                    ->validate()
+                        ->ifNotInArray($serializers)
+                        ->thenInvalid('The serializer %s is not supported. Please choose of of ' . json_encode($serializers))
+                    ->end()
+                ->end()
                 ->scalarNode('prefetch')->defaultNull()->end()
                 ->scalarNode('directory')->defaultValue('')->end()
                 ->arrayNode('queue_map')->end()
--- Tests/DependencyInjection/BernardExtensionTest.php
+++ Tests/DependencyInjection/BernardExtensionTest.php
@@ -13,6 +13,51 @@ class BernardExtensionTest extends \PHPUnit_Framework_TestCase
         $this->container = new ContainerBuilder;
     }
 
+    public function testInvalidDriver()
+    {
+        $this->setExpectedException('Symfony\Component\Config\Definition\Exception\InvalidConfigurationException');
+
+        $this->extension->load(array(array('driver' => 'invalid')), $this->container);
+    }
+
+    public function testInvalidSerializer()
+    {
+        $this->setExpectedException('Symfony\Component\Config\Definition\Exception\InvalidConfigurationException');
+
+        $this->extension->load(array(array('driver' => 'doctrine', 'serializer' => 'hopefully not valid')), $this->container);
+    }
+
+    public function testFileDriverRequiresDirectoryOptionToBeSet()
+    {
+        $this->setExpectedException('Symfony\Component\Config\Definition\Exception\InvalidConfigurationException');
+
+        $this->extension->load(array(array('driver' => 'file')), $this->container);
+    }
+
+    public function testDoctrinEventListenerIsAdded()
+    {
+        $this->extension->load(array(array('driver' => 'doctrine')), $this->container);
+
+        $definition = $this->container->getDefinition('bernard.schema_listener');
+
+        $this->assertTrue($definition->hasTag('doctrine.event_listener'));
+        $this->assertEquals(array(array(
+            'event' => 'postGenerateSchema',
+            'connection' => 'bernard',
+            'lazy' => true,
+        )), $definition->getTag('doctrine.event_listener'));
+    }
+
+    public function testDirectoryIsAddedToFileDriver()
+    {
+        $this->extension->load(array(array('driver' => 'file', 'directory' => __DIR__)), $this->container);
+
+        $definition = $this->container->getDefinition('bernard.driver.file');
+
+        $this->assertCount(1, $definition->getArguments());
+        $this->assertEquals(__DIR__, $definition->getArgument(0));
+    }
+
     public function testDefaultSerializer()
     {
         $this->extension->load(array(array('driver' => 'doctrine')), $this->container);
