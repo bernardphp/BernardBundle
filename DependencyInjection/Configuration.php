bernardphp/BernardBundle

commit 2fa3d50c9e58c60b29fdbfbfe6ee3eb469703632

use enumnode

DependencyInjection/Configuration.php
@@ -12,27 +12,18 @@ class Configuration implements \Symfony\Component\Config\Definition\Configuratio
         $tree = new TreeBuilder();
         $root = $tree->root('bernard_bernard');
 
-        $drivers = array('file', 'predis', 'doctrine');
-        $serializers = array('simple', 'jms', 'symfony');
-
         $root
             ->validate()
                 ->ifTrue(function ($v) { return 'file' === $v['driver'] && empty($v['options']['directory']); })
                 ->thenInvalid('The "directory" option must be defined when using the file driver.')
             ->end()
             ->children()
-                ->scalarNode('driver')
-                    ->validate()
-                        ->ifNotInArray($drivers)
-                        ->thenInvalid('The driver %s is not supported. Please choose one of ' . json_encode($drivers))
-                    ->end()
+                ->enumNode('driver')
+                    ->values(array('file', 'prefis', 'doctrine'))
                 ->end()
-                ->scalarNode('serializer')
+                ->enumNode('serializer')
                     ->defaultValue('simple')
-                    ->validate()
-                        ->ifNotInArray($serializers)
-                        ->thenInvalid('The serializer %s is not supported. Please choose of of ' . json_encode($serializers))
-                    ->end()
+                    ->values(array('jms', 'simple', 'symfony'))
                 ->end()
                 ->arrayNode('middlewares')
                     ->addDefaultsIfNotSet()

commit b3d90113a4b2ef467be3eeddfcc0334f530372b0

Fix predis typo (introduced in 2fa3d50c9e58c60b29fdbfbfe6ee3eb469703632)

DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements \Symfony\Component\Config\Definition\Configuratio
             ->end()
             ->children()
                 ->enumNode('driver')
-                    ->values(array('file', 'prefis', 'doctrine'))
+                    ->values(array('file', 'predis', 'doctrine'))
                 ->end()
                 ->enumNode('serializer')
                     ->defaultValue('simple')